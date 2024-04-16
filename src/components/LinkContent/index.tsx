2ec5799b4aafe19ad299de42f69dc7a335e4138d
Guilherme <gui.bellissimo@gmail.com>
2024-04-15 23:10:01 -0300

    insert saiba mais on links

--- src/components/LinkContent/index.tsx
+++ src/components/LinkContent/index.tsx
@@ -12,9 +12,7 @@ export function LinkContent() {
           <Button href="https://api.whatsapp.com/send?phone=5519995557295&text=Ol%C3%A1,%20gostaria%20de%20agendar%20uma%20consulta%20nutricional%20com%20o%20Dr.%20Alexandre%C2%A0Silva.">
             ATENDIMENTO ON-LINE
           </Button>
-          <Button href="https://api.whatsapp.com/send?phone=5519995557295&text=Ol%C3%A1,%20gostaria%20de%20agendar%20uma%20consulta%20nutricional%20com%20o%20Dr.%20Alexandre%C2%A0Silva.">
-            SAIBA COMO FUNCIONA
-          </Button>
+          <Button href="/saiba-mais">SAIBA COMO FUNCIONA</Button>
           <Button href="/">MENTORIA CONSULTÓRIO PRIME</Button>
           <Perfil />
         </Box>
