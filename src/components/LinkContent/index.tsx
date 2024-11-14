f7937cec047485b3702cf81c133dc71617ed03f7
Fabio <fabioluis07@hotmail.com>
2024-11-14 18:21:17 -0300

    adicionar id no botão

--- src/components/LinkContent/index.tsx
+++ src/components/LinkContent/index.tsx
@@ -6,14 +6,14 @@ export function LinkContent() {
       <Content>
         <Text>ALEXANDRE SILVA</Text>
         <Box>
-          <Button href="https://api.whatsapp.com/send?phone=5519995557295&text=Ol%C3%A1,%20gostaria%20de%20agendar%20uma%20consulta%20nutricional%20com%20o%20Dr.%20Alexandre%C2%A0Silva.">
+          <Button id="agendeSuaConsulta" href="https://api.whatsapp.com/send?phone=5519995557295&text=Ol%C3%A1,%20gostaria%20de%20agendar%20uma%20consulta%20nutricional%20com%20o%20Dr.%20Alexandre%C2%A0Silva.">
             AGENDE SUA CONSULTA
           </Button>
-          <Button href="https://api.whatsapp.com/send?phone=5519995557295&text=Ol%C3%A1,%20gostaria%20de%20agendar%20uma%20consulta%20nutricional%20com%20o%20Dr.%20Alexandre%C2%A0Silva.">
+          <Button id="atendimentoOnline" href="https://api.whatsapp.com/send?phone=5519995557295&text=Ol%C3%A1,%20gostaria%20de%20agendar%20uma%20consulta%20nutricional%20com%20o%20Dr.%20Alexandre%C2%A0Silva.">
             ATENDIMENTO ON-LINE
           </Button>
-          <Button href="/saiba-mais">SAIBA COMO FUNCIONA</Button>
-          <Button href="/">MENTORIA CONSULTÓRIO PRIME</Button>
+          <Button id="saibaMais" href="/saiba-mais">SAIBA COMO FUNCIONA</Button>
+          <Button id="consultoria" href="/">MENTORIA CONSULTÓRIO PRIME</Button>
           <Perfil />
         </Box>
       </Content>
